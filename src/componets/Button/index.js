466f437be6afda23cad69354af1e76e16b8c3f73
buidungFPT <139325583+buidungFPT@users.noreply.github.com>
2026-04-08 15:05:54 +0700

    ngay 1

---
+++ src/componets/Button/index.js
@@ -0,0 +1,7 @@
+
+import myAlert from '~/myAlert' ;
+
+function Button(){
+  return <button className="btn" onClick={myAlert}>Button</button>
+}
+export default Button;
---
+++ src/myAlert.js
@@ -0,0 +1,4 @@
+const myAlert = () => {
+    alert("Hello, this is an alert!");
+}
+export default myAlert;
